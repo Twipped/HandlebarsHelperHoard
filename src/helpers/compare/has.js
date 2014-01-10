8966d22842c75035e33c5c3e932901c77bd4ab75
ChiperSoft <chiper@chipersoft.com>
2014-01-10 14:20:36 -0800

    Renaming {{contains}} to {{has}} to free up for a string based {{contains}}

--- src/helpers/compare/has.js
+++ src/helpers/compare/has.js
@@ -1,13 +1,13 @@
 
 /**
- * {{contains}}
+ * {{has}}
  *
  * @param  {*}  input   [description]
  * @param  {*} value   [description]
  * @param  {Object} options [description]
  * @return {[type]}         [description]
  */
-exports.contains = function () {
+exports.has = function () {
 	return function (input, value, options) {
 		var result = input.indexOf(value) >= 0;
 		if (!options.fn) return result;
--- tests/compare/has.js
+++ tests/compare/has.js
@@ -3,22 +3,22 @@ var makeTests = require('../testBuilder.js');
 
 module.exports = makeTests([
 	{
-		template: '{{contains a b}}',
+		template: '{{has a b}}',
 		input: {a:[1,2,3], b:2},
 		output: 'true'
 	},
 	{
-		template: '{{contains a b}}',
+		template: '{{has a b}}',
 		input: {a:[1,2,3], b:0},
 		output: ''
 	},
 	{
-		template: '{{#contains a b}}yes{{else}}no{{/contains}}',
+		template: '{{#has a b}}yes{{else}}no{{/has}}',
 		input: {a:[1,2,3], b:1},
 		output: 'yes'
 	},
 	{
-		template: '{{#contains a b}}yes{{else}}no{{/contains}}',
+		template: '{{#has a b}}yes{{else}}no{{/has}}',
 		input: {a:[1,2,3], b:4},
 		output: 'no'
 	}
