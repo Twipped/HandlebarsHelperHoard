7dd99a612425ab6ab649b4af4513910f52e0938a
ChiperSoft <chiper@chipersoft.com>
2015-06-07 09:07:37 -0700

    Change default to test array truthiness

--- src/helpers/data/default.js
+++ src/helpers/data/default.js
@@ -1,12 +1,18 @@
 
 exports.default = function () {
+
+	function truthy (value) {
+		if (Array.isArray(value)) return value.length && value;
+		return value;
+	}
+
 	return function (value, fallback, options) {
 		options = arguments[arguments.length - 1];
 
-		if (arguments.length < 3) {
+		if (arguments.length !== 3) {
 			throw new Error('Handlebars Helper "default" needs 2 parameters');
 		}
 
-		return value || fallback;
+		return truthy(value) || fallback;
 	};
 };
